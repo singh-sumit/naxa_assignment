3e8c2d2d10ee9810d9199258626ccc58ef1c525e
singh-sumit <singhsumit9824@gmail.com>
2022-12-30 12:01:55 +0545

    [Task 2-completes] get Api with filtering projects

--- project_summarizer/api/v1/projects/serializers.py
+++ project_summarizer/api/v1/projects/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from project_summarizer.projects.models import Project
+from project_summarizer.projects.models import Project, Donor, Address, ImplementingPartner, CounterPartMinistry, \
+    ExecutingAgency, Sector
 
 
 class FileImportSerializer(serializers.Serializer):
@@ -11,3 +12,53 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+class DonorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donor
+        fields = ('name',)
+
+
+class ImplementingPartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImplementingPartner
+        fields = ('name', )
+
+class CounterPartMinistrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CounterPartMinistry
+        fields = ('name',)
+
+class ExecutingAgencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExecutingAgency
+        fields = ('name', )
+
+class ProjectLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = (
+            'province',
+            'district',
+            'municipality'
+        )
+
+class SectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = (
+            'code', 'name'
+        )
+
+
+class ProjectFilterSerializer(serializers.ModelSerializer):
+    donor = DonorSerializer()
+    executing_agency = ExecutingAgencySerializer()
+    implementing_partner = ImplementingPartnerSerializer()
+    counterpart_ministry = CounterPartMinistrySerializer()
+    project_location = ProjectLocationSerializer()
+    sector = SectorSerializer()
+
+    class Meta:
+        model = Project
+        exclude = ('id',)
--- project_summarizer/api/v1/projects/views.py
+++ project_summarizer/api/v1/projects/views.py
@@ -1,15 +1,44 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from project_summarizer.api.v1.projects.serializers import FileImportSerializer, ProjectSerializer
+from project_summarizer.api.v1.projects.serializers import FileImportSerializer, ProjectSerializer, \
+    ProjectFilterSerializer
+from project_summarizer.config.core.utils.query_params import queryparams_to_Q
 from project_summarizer.config.core.utils.read_write_files import process_uploaded_file
+from project_summarizer.projects.models import Project
 
 
-class ProjectViewSet(GenericViewSet):
+class ProjectViewSet(GenericViewSet, ListModelMixin):
     serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+    def get_queryset(self):
+        # sector_name = self.request.query_params.get('sector_name')
+        # ministry_name = self.request.query_params.get('ministry')
+        # project_status = self.request.query_params.get('project_status')
+
+        filter_conditions = queryparams_to_Q(params_qs=self.request.query_params)
+        print(filter_conditions, type(filter_conditions))
+
+        if filter_conditions:
+            queryset = Project.objects.filter(
+                # sector__name__icontains=sector_name,
+                # counterpart_ministry__name__icontains=ministry_name,
+                # status__iexact=project_status,
+                filter_conditions
+            ).all()
+            print("QS", queryset)
+            return queryset
+        return super().get_queryset()
+
+    # def get_serializer(self, *args, **kwargs):
+    #     if self.action == 'list':
+    #         return ProjectFilterSerializer
+    #     return super().get_serializer(*args, **kwargs)
 
     @extend_schema(request=FileImportSerializer)
     @action(detail=False, url_path='upload-project', name='Import Project', methods=['POST'])
@@ -19,3 +48,16 @@ class ProjectViewSet(GenericViewSet):
         if file := request.data.get('file'):
             return process_uploaded_file(file)
         return Response('Project data import failed', status=status.HTTP_400_BAD_REQUEST)
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = ProjectFilterSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, url_path='project-summary', name='Project Summary', methods=['GET'])
+    def project_summary(self, request, *args, **kwargs):
+        return Response('working')
+
+
